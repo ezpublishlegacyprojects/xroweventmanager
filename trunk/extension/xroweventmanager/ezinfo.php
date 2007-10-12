485351c3fbb1e41fae0a842176acb45380d71119
Björn Dieding <bjoern@xrow.de>
2007-10-12 18:39:16 +0000

    info details
    git-svn-id: http://svn.projects.ez.no/xroweventmanager@2 6d8e04b3-daa3-4749-af31-cdae7c5a2f0a

---
+++ trunk/extension/xroweventmanager/ezinfo.php
@@ -0,0 +1,13 @@
+<?php
+class xroweventManagerInfo
+{
+    function info()
+    {
+        return array( 'Name' => "xrow Eventmanage3r",
+                      'Version' => "1.0",
+                      'Copyright' => "Copyright (C) xrow Gbr",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+?>
