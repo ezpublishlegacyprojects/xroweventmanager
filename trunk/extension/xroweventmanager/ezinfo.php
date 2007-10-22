107e5c7e611ed3644e879b7e75f32c8a76fe0d02
Björn Dieding <bjoern@xrow.de>
2007-10-22 20:19:16 +0000

    - typo
    git-svn-id: http://svn.projects.ez.no/xroweventmanager@9 6d8e04b3-daa3-4749-af31-cdae7c5a2f0a

--- trunk/extension/xroweventmanager/ezinfo.php
+++ trunk/extension/xroweventmanager/ezinfo.php
@@ -6,7 +6,7 @@ class xroweventmanagerInfo
         return array( 'Name' => "xrow eventmanager",
                       'Version' => "1.0",
                       'Copyright' => "Copyright (C) xrow GbR",
-                      'Author' => 'Georg Franz'
+                      'Author' => 'Georg Franz',
                       'License' => "GNU General Public License v2.0"
                      );
     }
